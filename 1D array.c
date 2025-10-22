97e18ac8e1cd613847c25a2b52312330b06bc0c2
avimalliksaikat <avimalliksaikat@gmail.com>
2025-10-23 02:08:58 +0600

    Add files via upload

---
+++ 1D array.c
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+const int n = 6 ;
+void traverse(int arr1[n])
+{
+    cout
+    for(int i=0;i<n;i++)
+    {
+        cout << arr1[i] << " "
+    }
+}
+int main()
+{
+    int arr1[n]={1,2,3,4,5,6};
+    int arr2[n]={2,3,4,5,6,7};
+
+    traverse(arr1);
+}
